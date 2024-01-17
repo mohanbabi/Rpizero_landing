ee0e319920e49194c4493c1b53530dfa17bc6f67
mohan <mohanbabi16@gmail.com>
2024-01-17 22:27:22 +0530

    17-01-2024(need to test)
    17-01-2024(need to test)

---
+++ 17-01-2024(need to test).py
@@ -0,0 +1,165 @@
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+if sys.version_info.major == 3 and sys.version_info.minor >= 10:
+    import collections
+    from collections.abc import MutableMapping
+    setattr(collections, "MutableMapping", MutableMapping)
+
+import time
+import math
+import argparse
+
+
+from dronekit import connect, VehicleMode, LocationGlobalRelative, Command, LocationGlobal
+from pymavlink import mavutil
+from opencv.lib_aruco_pose import *
+
+parser = argparse.ArgumentParser()
+#-----replace udp:192.168.29.235:14554 with your drone mavlink data
+parser.add_argument('--connect', default = 'udp:192.168.29.235:3000')
+args = parser.parse_args()
+
+def get_location_metres(original_location, dNorth, dEast):
+    """
+    Returns a Location object containing the latitude/longitude `dNorth` and `dEast` metres from the
+    specified `original_location`. The returned Location has the same `alt and `is_relative` values
+    as `original_location`.
+
+    The function is useful when you want to move the vehicle around specifying locations relative to
+    the current vehicle position.
+    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+    For more information see:
+    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+    """
+    earth_radius=6378137.0 #Radius of "spherical" earth
+    #Coordinate offsets in radians
+    dLat = dNorth/earth_radius
+    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+    
+    print ("dlat, dlon", dLat, dLon)
+
+    #New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180/math.pi)
+    newlon = original_location.lon + (dLon * 180/math.pi)
+    return(newlat, newlon)
+
+
+def marker_position_to_angle(x, y, z):
+    
+    angle_x = math.atan2(x,z)
+    angle_y = math.atan2(y,z)
+    
+    return (angle_x, angle_y)
+
+
+def camera_to_uav(x_cam, y_cam):
+    x_uav =-y_cam
+    y_uav = x_cam
+    return(x_uav, y_uav)
+
+
+def uav_to_ne(x_uav, y_uav, yaw_rad):
+    c       = math.cos(yaw_rad)
+    s       = math.sin(yaw_rad)
+    
+    north   = x_uav*c - y_uav*s
+    east    = x_uav*s + y_uav*c 
+    return(north, east)
+
+def check_angle_descend(angle_x, angle_y, angle_desc):
+    return(math.sqrt(angle_x**2 + angle_y**2) <= angle_desc)
+
+
+
+def send_landing(x,y,z):
+    # Create landing target MAVLink message
+    msg = vehicle.message_factory.landing_target_encode(
+        0,  # time since system boot, not used
+        0,  # target num, not used
+        mavutil.mavlink.MAV_FRAME_BODY_NED,  # frame, not used
+        x,  # convert angle to 1e-4 radians
+        y,  # convert angle to 1e-4 radians
+        z,  # convert distance to 1e-2 centimeters
+        0,  # Target x-axis size, in radians (not used)
+        0  # Target y-axis size, in radians (not used)
+    )
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+        
+
+
+#--------------------------------------------------
+#-------------- CONNECTION  
+#--------------------------------------------------    
+#-- Connect to the vehicle
+print('Connecting...')
+vehicle = connect(args.connect)  
+
+
+
+
+#--------------------------------------------------
+#-------------- PARAMETERS  
+#-------------------------------------------------- 
+rad_2_deg   = 180.0/math.pi
+deg_2_rad   = 1.0/rad_2_deg 
+
+#--------------------------------------------------
+#-------------- LANDING MARKER  
+#--------------------------------------------------    
+#--- Define Tag
+id_to_find      = 72
+marker_size     = 10 #- [cm]
+freq_send       = 1 #- Hz
+
+land_alt_cm         = 50.0
+angle_descend       = 20*deg_2_rad
+land_speed_cms      = 30.0
+
+#--- Get the camera calibration path
+# Find full directory path of this script, used for loading config and other files
+cwd                 = path.dirname(path.abspath(__file__))
+calib_path          = "/home/pi/how_do_drones_work/opencv/"
+camera_matrix       = np.loadtxt(calib_path+'cameraMatrix_raspi.txt', delimiter=',')
+camera_distortion   = np.loadtxt(calib_path+'cameraDistortion_raspi.txt', delimiter=',')                                      
+aruco_tracker       = ArucoSingleTracker(id_to_find=id_to_find, marker_size=marker_size, show_video=False, 
+                camera_matrix=camera_matrix, camera_distortion=camera_distortion)
+                
+
+
+time_0 = time.time()
+
+while True:                
+
+    marker_found, x_cm, y_cm, z_cm = aruco_tracker.track(loop=False)
+    if marker_found:
+        x_cm, y_cm          = camera_to_uav(x_cm, y_cm)
+        uav_location        = vehicle.location.global_relative_frame
+        
+        #-- If high altitude, use baro rather than visual
+        if uav_location.alt >= 5.0:
+            print (z_cm = (uav_location.alt*100.0))
+        angle_x, angle_y = marker_position_to_angle(x_cm, y_cm, z_cm)
+
+        
+        if time.time() >= time_0 + 1.0/freq_send:
+            time_0 = time.time()
+            # print ""
+           # print " "
+            print ("Altitude = %.0fcm"%z_cm)
+            print ("Marker found x = %5.0f cm  y = %5.0f cm -> angle_x = %5f  angle_y = %5f"%(x_cm, y_cm, angle_x*rad_2_deg, angle_y*rad_2_deg))
+            
+            north, east             = uav_to_ne(x_cm, y_cm, vehicle.attitude.yaw)
+            print ("Marker N = %5.0f cm   E = %5.0f cm   Yaw = %.0f deg"%(north, east, vehicle.attitude.yaw*rad_2_deg))
+            
+            send_landing (north,east, (z_cm/100))
+            vehicle.flush()
+            
+            
+        #--- COmmand to land
+        if z_cm <= land_alt_cm:
+            if vehicle.mode == "GUIDED":
+                print (" -->>COMMANDING TO LAND<<")
+                vehicle.mode = "LAND"
+            
